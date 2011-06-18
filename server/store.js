a2eb076cd74d4d5cfb570c4666c312b3a823d8c3
Benjamin Horsleben <benjamin@horsleben.com>
2011-06-18 15:22:20 +0200

    Added a .gitignore to remove the .DS_Store files from git. Added a store that supports storing the participant-data. Currently it does not store the data persistently, but the store does support reading to/writing from a json file.

---
+++ server/store.js
@@ -0,0 +1,109 @@
+var fs = require('fs');
+
+(function store(exports) {
+	var loading = false,
+		data = null,
+		listeners = [];
+	
+	function listen(listener) {
+		listeners.push(listener);
+	};
+	function load() {
+		if(!data && !loading) {
+			fs.readFile('data.json', dataLoaded);
+			loading = true;
+		}
+	};
+	var waitingForSave = false;
+	
+	function createDefaultData() {
+		data = {
+			entries: []
+		};
+	};
+	
+	function save() {
+		return;
+		if(!waitingForSave) {
+			waitingForSave = true;
+			setTimeout(function() {
+				waitingForSave = false;
+				fs.writeFile('data.json', JSON.stringify(data));
+			}, 2000);
+		}
+	};
+	function dataLoaded(err, contents) {
+		if(err) {
+			createDefaultData();
+			save();
+			return;
+		}
+		data = JSON.parse(contents);
+		exports.data = data;
+	};
+	
+	(function entries() {
+		function getEntries() {
+			return data.entries;
+		};
+		
+		function getEntry(name) {
+			var e;
+			data.entries.some(function iterator(entry) {
+				if(entry.name == name) {
+					e = entry;
+					return true;
+				}
+				return false;
+			});
+			return e;
+		};
+		function addEntry(name) {
+			var entry = getEntry(name);
+			if(!entry) {
+				entry = {
+					name: name,
+					coords: {}
+				};
+				
+				data.entries.push(entry);
+				save();
+			}
+			return entry;
+		};
+		function updateEntry(name, data) {
+			var entry = addEntry(name);
+			Object.keys(data).forEach(function updater(key) {
+				entry[key] = data[key];
+			});
+			save();
+			return entry;
+		};
+		
+		function deleteEntry(name) {
+			var index;
+			data.entries.some(function iterator(entry, i) {
+				if(entry.name == name) {
+					index = i;
+					return true;
+				}
+				return false;
+			});
+			if(index != null) {
+				data.entries.splice(index, 1);
+			}
+		};
+		
+		exports.deleteEntry = deleteEntry;
+		exports.addEntry = addEntry;
+		exports.getEntries = getEntries;
+		exports.getEntry = getEntry;
+		exports.updateEntry = updateEntry;
+	})();
+	
+	exports.data = data;
+	exports.listen = listen;
+	exports.load = load;
+	exports.save = save;
+	exports.dataLoaded = dataLoaded;
+})(this);
